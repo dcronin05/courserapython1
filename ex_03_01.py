3c27e485e2fd70e7695eaf9ffc4eb1e5753d75d6
dcronin05 <37851794+dcronin05@users.noreply.github.com>
2019-01-20 22:37:58 -0700

    Added computepay() for Assignment 3.1

--- ex_03_01.py
+++ ex_03_01.py
@@ -16,11 +16,31 @@ def aMethod():
     print(input())
 
 
-def score(score = float(input("Enter Score: "))):
+def score():
     
-    while not score <= 1.0:
-        print("The score is out of range.")
+    while True:
         score = float(input("Enter Score: "))
+        if score >= 0.0 and score <= 1.0:
+            if score >= 0.9:
+                print("A")
+            elif score >= 0.8:
+                print("B")
+            elif score >= 0.7:
+                print("C")
+            else:
+                print("D")
+            break
+        else:
+            print("Incorrect score.")
 
 
-score()
+def computepay(hr, 
+    rate):
+    
+    if hr > 40:
+        return (hr - 40) * (rate * 1.5) + (40 * rate)
+    else:
+        return hr * rate
+
+
+print(computepay(int(input("Enter hours: ")), float(input("Enter rate: "))))
